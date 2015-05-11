fd629dc3dcbec1e8a26f9ac66bdc9a31f51cb78c
jocloud31 <jocloud31@live.com>
2015-05-11 10:00:15 -0500

    Update game.py

--- game.py
+++ game.py
@@ -4,7 +4,7 @@ from colors import *
 
 class Block(pygame.sprite.Sprite):
 	def _init__(self, color=bleu, width=50, height=50):
-		super(Block, self).__init__()
+		super(self.__class__, self).__init__()
 
 		self.image = pygame.Surface((width, height))
 
