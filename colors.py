6e926de67294c2bf3fc5e0a8a3859648b59cab04
Notafryingpan_games <jocloud31@live.com>
2015-05-11 00:46:13 -0500

    Initial Actual Commit
    Beginnings of a platformer for funzies.

---
+++ colors.py
@@ -0,0 +1,8 @@
+__author__ = 'Jay'
+import pygame
+
+white = pygame.Color(255, 255, 255)
+black = pygame.Color(0, 0, 0)
+sky = pygame.Color(100, 100, 255)
+bleu = pygame.Color(40, 50, 200)
+grass = pygame.Color(15, 200, 40)
---
+++ game.py
@@ -0,0 +1,46 @@
+import pygame
+from colors import *
+
+
+class Block(pygame.sprite.Sprite):
+	def _init__(self, color=bleu, width=50, height=50):
+		super(Block, self).__init__()
+
+		self.image = pygame.Surface((width, height))
+
+		self.image.fill(color)
+
+		self.rect = self.image.get_rect()
+
+if __name__ == "__main__":
+	pygame.init()
+
+	window_size = window_width, window_height = 640, 480
+	window = pygame.display.set_mode(window_size, pygame.RESIZABLE)
+
+	pygame.display.set_caption("Bleu Blur")
+
+	window.fill(sky)
+
+	clock = pygame.time.Clock()
+	FPS = 60
+
+	block_group = pygame.sprite.Group()
+
+	sanic = Block()
+	block_group.add(sanic)
+
+	block_group.draw(window)
+
+	running = True
+
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
+		clock.tick(FPS)
+		pygame.display.update()
+
+	pygame.quit()
+	quit()
