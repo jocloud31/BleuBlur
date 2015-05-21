d5d4f398ff0748fa45cdde1e9ac29c17d3ece412
Notafryingpan_games <jocloud31@live.com>
2015-05-21 02:19:08 -0500

    Redone mostly from scratch to no longer follow previous tutorial. Don't mind the deceleration bug.

--- game.py
+++ game.py
@@ -4,14 +4,46 @@ from colors import *
 
 
 class Block(Sprite):
-	def __init__(self, color=bleu, width=50, height=50):
+	def __init__(self, color, size):
 		Sprite.__init__(self)
 
-		self.image = pygame.Surface((width, height))
-
-		self.image.fill(color)
+		self.color = color
+		self.image = pygame.Surface(size)
+		self.image.fill(self.color)
 
 		self.rect = self.image.get_rect()
+		self.hspeed = float(0)
+		self.vspeed = 0.0
+		self.target_hspeed = float(0)
+		self.target_vspeed = 0.0
+		block_group.append(self)
+
+	def change_speed(self, hspeed_change, vspeed_change):
+		self.target_hspeed += hspeed_change
+		self.target_vspeed += vspeed_change
+
+	def set_position(self, x, y):
+		self.rect.x = x
+		self.rect.y = y
+
+	def update_block(self):
+		self.hspeed += (self.target_hspeed - self.hspeed) * .01
+		self.vspeed += (self.target_vspeed - self.vspeed) * .01
+		"""if -.1 < self.hspeed < .1:
+			self.hspeed = 0
+		if -.1 < self.vspeed < .1:
+			self.vspeed = 0"""
+		self.rect.x += self.hspeed
+		self.rect.y += self.vspeed
+		self.set_position(self.rect.x, self.rect.y)
+		print self.hspeed
+		print self.vspeed
+
+	def draw_to_screen(self):
+		window.blit(self.image, [self.rect.x, self.rect.y])
+
+	def gravity_falls(self):
+		pass
 
 if __name__ == "__main__":
 	pygame.init()
@@ -26,12 +58,16 @@ if __name__ == "__main__":
 	clock = pygame.time.Clock()
 	FPS = 60
 
-	block_group = pygame.sprite.Group()
+	block_group = []
 
-	sanic = Block()
-	block_group.add(sanic)
+	sky_box = Block(sky, [window_width, window_height])
+	sky_box.set_position(0, 0)
 
-	block_group.draw(window)
+	ground = Block(grass, [window_width, 15])
+	ground.set_position(0, window_height - 15)
+
+	sanic = Block(bleu, [25, 25])
+	sanic.set_position(5, window_height - 40)
 
 	running = True
 
@@ -40,7 +76,39 @@ if __name__ == "__main__":
 			if event.type == pygame.QUIT:
 				running = False
 
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					running = False
+
+				if event.key == pygame.K_w:
+					sanic.change_speed(0, -5)
+
+				if event.key == pygame.K_s:
+					sanic.change_speed(0, 5)
+
+				if event.key == pygame.K_a:
+					sanic.change_speed(-5, 0)
+
+				if event.key == pygame.K_d:
+					sanic.change_speed(5, 0)
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_w:
+					sanic.target_vspeed = 0
+
+				if event.key == pygame.K_s:
+					sanic.target_vspeed = 0
+
+				if event.key == pygame.K_a:
+					sanic.target_hspeed = float(0)
+
+				if event.key == pygame.K_d:
+					sanic.target_hspeed = float(0)
+
 		clock.tick(FPS)
+		sanic.update_block()
+		for item in block_group:
+			item.draw_to_screen()
 		pygame.display.update()
 
 	pygame.quit()
